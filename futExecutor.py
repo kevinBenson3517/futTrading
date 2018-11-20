5f3d9651c91ffb1529fd645b04807d76a2f867b9
Kevin Benson <benson_k3@denison.edu>
2018-11-19 20:38:17 -0800

    Add library and wrapper func

---
+++ futExecutor.py
@@ -0,0 +1,29 @@
+import fut
+import time
+import futScript
+from time import sleep
+from threading import Thread
+
+def MAX_TPS_HOUR = 500
+def MAX_TPS_DAY = 5000
+# Wrapper for the fut script to run for specified time. 
+def run_fut(duration):
+    # Point to 
+
+int maxTPSHour
+hoursToRun = int(raw_input('Enter the amount of hours you want to run this: '))
+if hoursToRun <= 10:
+    maxTPSHour = MAX_TPS_HOUR
+if 10 < hoursToRun < 24:
+    maxTPSHour = MAX_TPS_DAY/hoursToRun
+if 24 < hoursToRun:
+    maxTPSHour = 208
+
+t = Thread(target=run_fut, args=(maxTPSHour))   # run the fut func in another thread. 
+t.daemon = True                                 # Python will exit when the main thread
+                                                # exits, even if this thread is still
+                                                # running
+
+t.start()
+secondsToRun = hoursToRun*3600
+sleep(secondsToRun)
